08b021f1387699a1f8073e7f3533616136fffa0a
Peter_Svirid <Peter.blr@yandex.ru>
2021-04-26 17:22:47 +0300

    Procedures

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,7 +6,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1, y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 5, y2 = 7;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+            Console.ReadKey();
+        }
+
+        private static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
