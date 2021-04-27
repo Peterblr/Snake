9ab3726f925191eed9ec0ec41a384aa1a4958d43
Peter_Svirid <Peter.blr@yandex.ru>
2021-04-27 20:58:27 +0300

    add move key

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -19,28 +19,24 @@ namespace Snake
             line_Left.Draw();
             VerticalLine line_Reight = new VerticalLine(0, 24, 78, 'o');
             line_Reight.Draw();
-            Console.ReadKey();
 
             Point p = new Point(4, 5, '#');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
-            snake.Move();
-            Thread.Sleep(300); 
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
-            snake.Move();
-            Thread.Sleep(300);
+            
 
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                    
+                }
+                Thread.Sleep(150);
+                snake.Move();
+
+            }
 
 
         }
