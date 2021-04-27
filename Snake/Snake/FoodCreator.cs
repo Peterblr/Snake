8a29d4d14ce9edfc0bd516d43c1c878b52b3bd0c
Peter_Svirid <Peter.blr@yandex.ru>
2021-04-27 21:53:45 +0300

    add food

---
+++ Snake/Snake/FoodCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Snake
+{
+    class FoodCreator
+    {
+        int mapWidth1;
+        int mapHeigth1;
+        char sym;
+
+        Random random = new Random();
+
+
+        public FoodCreator(int mapWidth, int mapHeigth, char sym)
+        {
+            this.mapWidth1 = mapWidth;
+            this.mapHeigth1 = mapHeigth;
+            this.sym = sym;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth1 - 4);
+            int y = random.Next(2, mapHeigth1 - 4);
+            return new Point(x, y, sym);
+        }
+    }
+}
